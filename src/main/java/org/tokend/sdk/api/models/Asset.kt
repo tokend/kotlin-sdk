e0acd8cca299357b804b1a8ec2f8a33a64274a0a
Oleg Koretsky <test.radiokot@gmail.com>
2018-05-17 09:47:33 +0300

    Remove unnecessary nullables from asset model

--- src/main/java/org/tokend/sdk/api/models/Asset.kt
+++ src/main/java/org/tokend/sdk/api/models/Asset.kt
@@ -7,16 +7,16 @@ import java.math.BigDecimal
  * Created by Oleg Koretsky on 1/4/18.
  */
 data class Asset(
-        @SerializedName("code") val code: String?,
-        @SerializedName("owner") val ownerAccount: String?,
-        @SerializedName("policy") val policy: Int?,
-        @SerializedName("details") val details: Details?,
-        @SerializedName("issued") val issued: BigDecimal?,
+        @SerializedName("code") val code: String,
+        @SerializedName("owner") val ownerAccount: String,
+        @SerializedName("policy") val policy: Int,
+        @SerializedName("details") val details: Details,
+        @SerializedName("issued") val issued: BigDecimal,
         @SerializedName("sales") val sales: List<Sale>? = null) {
 
     class Details(
-            @SerializedName("name") val name: String?,
-            @SerializedName("logo") val logo: RemoteFile?,
-            @SerializedName("terms") val terms: RemoteFile?
+            @SerializedName("name") val name: String,
+            @SerializedName("logo") val logo: RemoteFile,
+            @SerializedName("terms") val terms: RemoteFile
     )
 }
