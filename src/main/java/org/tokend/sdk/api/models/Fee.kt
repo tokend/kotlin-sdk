a45a8179ef108f9c5fa3bf1cbcfa999c09d2d301
Oleg Koretsky <test.radiokot@gmail.com>
2018-05-17 09:47:19 +0300

    Made fee model serializable

--- src/main/java/org/tokend/sdk/api/models/Fee.kt
+++ src/main/java/org/tokend/sdk/api/models/Fee.kt
@@ -2,16 +2,15 @@ package org.tokend.sdk.api.models
 
 import com.google.gson.annotations.SerializedName
 import org.tokend.sdk.utils.BigDecimalUtil
+import java.io.Serializable
 import java.math.BigDecimal
 
-/**
- * Created by Oleg Koretsky on 11/28/17.
- */
-data class Fee(@SerializedName("fee_type") val feeType: Int,
-          @SerializedName("asset") val asset: String,
-          @SerializedName("fixed") private val fixedString: String,
-          @SerializedName("percent") private val percentString: String,
-          @SerializedName("lower_bound") private val lowBoundString: String) {
+data class Fee(
+        @SerializedName("fee_type") val feeType: Int,
+        @SerializedName("asset") val asset: String,
+        @SerializedName("fixed") private val fixedString: String,
+        @SerializedName("percent") private val percentString: String,
+        @SerializedName("lower_bound") private val lowBoundString: String) : Serializable {
 
     val fixed: BigDecimal
         get() = BigDecimalUtil.valueOf(fixedString)
@@ -19,4 +18,6 @@ data class Fee(@SerializedName("fee_type") val feeType: Int,
         get() = BigDecimalUtil.valueOf(percentString)
     val lowerBound: BigDecimal
         get() = BigDecimalUtil.valueOf(lowBoundString)
+    val total: BigDecimal
+        get() = fixed + percent
 }
