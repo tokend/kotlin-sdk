6b7f1c1ba5ef194b43f3b75416a450704672b8fe
Oleg Koretsky <test.radiokot@gmail.com>
2018-07-30 17:37:29 +0300

    Enabled all SSL ciphers in HTTP client factory

--- src/main/java/org/tokend/sdk/factory/HttpClientFactory.kt
+++ src/main/java/org/tokend/sdk/factory/HttpClientFactory.kt
@@ -1,6 +1,9 @@
 package org.tokend.sdk.factory
 
-import okhttp3.*
+import okhttp3.ConnectionSpec
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.TlsVersion
 import okhttp3.logging.HttpLoggingInterceptor
 import org.tokend.sdk.api.requests.CookieJarProvider
 import java.util.*
@@ -23,10 +26,7 @@ class HttpClientFactory {
 
         val connectionSpec = ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
                 .tlsVersions(TlsVersion.TLS_1_2)
-                .cipherSuites(
-                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-                        CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256)
+                .allEnabledCipherSuites()
                 .build()
 
         clientBuilder.apply {
