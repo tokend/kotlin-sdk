6455364f8baffe25877c23038f19cca13fb476e0
Oleg Koretsky <test.radiokot@gmail.com>
2019-01-04 15:26:36 +0200

    Added extra assets v2 query params

--- src/main/java/org/tokend/sdk/api/v2/assets/params/AssetsPageParams.kt
+++ src/main/java/org/tokend/sdk/api/v2/assets/params/AssetsPageParams.kt
@@ -2,11 +2,14 @@ package org.tokend.sdk.api.v2.assets.params
 
 import org.tokend.sdk.api.base.params.PagingParamsHolder
 import org.tokend.sdk.api.base.params.PagingParamsV2
+import org.tokend.sdk.utils.extentions.bitmask
+import org.tokend.wallet.xdr.AssetPolicy
 
 /**
  * @see AssetParams.Includes
  */
 open class AssetsPageParams(
+        val policies: Collection<AssetPolicy>? = null,
         include: Collection<String>? = null,
         val pagingParams: PagingParamsV2? = null
 ) : AssetParams(include), PagingParamsHolder {
@@ -16,6 +19,9 @@ open class AssetsPageParams(
 
     override fun map(): Map<String, Any> {
         return super.map().toMutableMap().apply {
+            policies?.also {
+                put("policy", policies.map { it.value.toLong() }.bitmask())
+            }
             pagingParams?.also { putAll(it.map()) }
         }
     }
