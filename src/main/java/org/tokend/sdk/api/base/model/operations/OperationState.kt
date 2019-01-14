98654bddb6c3fdf96636ad6736d9da304827eaf6
Oleg Koretsky <test.radiokot@gmail.com>
2019-01-14 12:54:31 +0200

    Added parsing partially matched offers as successfull

--- src/main/java/org/tokend/sdk/api/base/model/operations/OperationState.kt
+++ src/main/java/org/tokend/sdk/api/base/model/operations/OperationState.kt
@@ -9,11 +9,12 @@ enum class OperationState(val literal: String) {
 
     companion object {
         const val FULLY_MATCHED_LITERAL = "fully matched"
+        const val PARTIALLY_MATCHED_LITERAL = "partially matched"
 
         @JvmStatic
         fun fromLiteral(literal: String?): OperationState {
             literal ?: return PENDING
-            if (literal == FULLY_MATCHED_LITERAL) {
+            if (literal == FULLY_MATCHED_LITERAL || literal == PARTIALLY_MATCHED_LITERAL) {
                 return SUCCESS
             }
             return values().find { it.literal == literal } ?: PENDING
