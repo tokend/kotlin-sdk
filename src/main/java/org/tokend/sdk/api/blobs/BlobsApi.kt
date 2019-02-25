cca55d1ddeb2c2f032964b610fd40fb10087db2f
Aleksandr <al.ivanchukov@gmail.com>
2019-02-25 14:07:03 +0200

    Actialize blob api

--- src/main/java/org/tokend/sdk/api/blobs/BlobsApi.kt
+++ src/main/java/org/tokend/sdk/api/blobs/BlobsApi.kt
@@ -2,35 +2,73 @@ package org.tokend.sdk.api.blobs
 
 import org.tokend.sdk.api.base.ApiRequest
 import org.tokend.sdk.api.base.MappedRetrofitApiRequest
-import org.tokend.sdk.api.blobs.params.BlobsParams
+import org.tokend.sdk.api.base.SimpleRetrofitApiRequest
+import org.tokend.sdk.api.base.model.DataEntity
+import org.tokend.sdk.api.base.params.map
 import org.tokend.sdk.api.blobs.model.Blob
+import org.tokend.sdk.api.blobs.params.BlobPageParams
 
 open class BlobsApi(
         protected val blobsService: BlobsService
 ) {
+
     /**
-     * Will return list of blobs filtered with given params
-     * @see <a href="https://tokend.gitlab.io/docs/?http#filter-blobs">Docs</a>
+     * @return list of blobs owned by specific account.
      */
-    fun get(accountId: String,
-            params: BlobsParams? = null): ApiRequest<List<Blob>> {
+    fun getAccountOwnedBlobs(accountId: String, params: BlobPageParams? = null): ApiRequest<List<Blob>> {
         return MappedRetrofitApiRequest(
-                blobsService.getBlobs(
-                        accountId,
-                        params?.map()
-                ),
+                blobsService.getAccountOwnedBlobs(accountId, params.map()),
                 { it.data }
         )
     }
 
     /**
-     * Will return specific blob by it's id.
-     * @see <a href="https://tokend.gitlab.io/docs/?http#get">Docs</a>
+     * Will create blob for specific account.
      */
-    fun getById(accountId: String,
-                blobId: String): ApiRequest<Blob> {
+    fun createAccountOwnedBlob(accountId: String, blob: Blob): ApiRequest<Blob> {
         return MappedRetrofitApiRequest(
-                blobsService.getBlob(accountId, blobId),
+                blobsService.creteAccountOwnedBlob(accountId, DataEntity(blob)),
+                { it.data }
+        )
+    }
+
+    /**
+     * @return specific blob by it's id.
+     */
+    fun getBlob(blobId: String): ApiRequest<Blob> {
+        return MappedRetrofitApiRequest(
+                blobsService.get(blobId),
+                { it.data }
+        )
+    }
+
+    /**
+     * @return specific blob owned by specific account.
+     */
+    fun getAccountOwnedBlob(accountId: String,
+                            blobId: String): ApiRequest<Blob> {
+        return MappedRetrofitApiRequest(
+                blobsService.getAccountOwnedBlob(accountId, blobId),
+                { it.data }
+        )
+    }
+
+    /**
+     * Will delete specific blob.
+     */
+    fun delete(blobId: String): ApiRequest<Void> {
+        return SimpleRetrofitApiRequest(
+                blobsService.delete(blobId)
+        )
+    }
+
+
+    /**
+     * Will create blob.
+     */
+    fun crete(blob: Blob): ApiRequest<Blob> {
+        return MappedRetrofitApiRequest(
+                blobsService.create(DataEntity(blob)),
                 { it.data }
         )
     }
---
+++ src/main/java/org/tokend/sdk/api/blobs/params/BlobPageParams.kt
@@ -0,0 +1,44 @@
+package org.tokend.sdk.api.blobs.params
+
+import org.tokend.sdk.api.base.params.PagingParamsV2
+import org.tokend.sdk.api.v3.base.PageQueryParams
+
+open class BlobPageParams(
+        val type: String? = null,
+        val isDeleted: Boolean? = null,
+        pagingParamsV2: PagingParamsV2? = null,
+        include: Collection<String>? = null
+) : PageQueryParams(pagingParamsV2, include) {
+    override fun map(): Map<String, Any> {
+        return mutableMapOf<String, Any>()
+                .apply {
+                    type?.also { putFilter("type", it) }
+                    isDeleted?.also { putFilter("deleted", it) }
+                }
+    }
+
+    class Builder: PageQueryParams.Builder() {
+        private var type: String? = null
+        private var isDeleted: Boolean? = null
+
+        fun withType(type: String) = also { this.type = type }
+
+        fun withIsDeleted(isDeleted: Boolean) = also { this.isDeleted = isDeleted }
+
+        override fun withPagingParams(pagingParams: PagingParamsV2) = also {
+            super.withPagingParams(pagingParams)
+        }
+
+        override fun withInclude(include: Collection<String>?) = also {
+            super.withInclude(include)
+        }
+
+        override fun withInclude(vararg include: String) = also {
+            super.withInclude(*include)
+        }
+
+        override fun build(): BlobPageParams {
+            return BlobPageParams(type, isDeleted, pagingParams, include)
+        }
+    }
+}
