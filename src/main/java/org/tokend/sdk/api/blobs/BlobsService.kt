eb43eedf1489fe288ae613d056240d5ef5335f3d
Aleksandr <al.ivanchukov@gmail.com>
2019-02-25 14:55:02 +0200

    Actialize blob api (#2)
    * Actialize blob api
    
    * Fix page params map() return

--- src/main/java/org/tokend/sdk/api/blobs/BlobsService.kt
+++ src/main/java/org/tokend/sdk/api/blobs/BlobsService.kt
@@ -3,17 +3,29 @@ package org.tokend.sdk.api.blobs
 import org.tokend.sdk.api.blobs.model.Blob
 import org.tokend.sdk.api.base.model.DataEntity
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.http.QueryMap
+import retrofit2.http.*
 
 interface BlobsService {
-    @GET("users/{accountId}/blobs/{blobId}")
-    fun getBlob(@Path("accountId") accountId: String?,
-                @Path("blobId") blobId: String?): Call<DataEntity<Blob>>
 
-    @GET("users/{accountId}/blobs")
+    @GET("accounts/{account}/blobs")
     @JvmSuppressWildcards
-    fun getBlobs(@Path("accountId") accountId: String,
-                 @QueryMap query: Map<String, Any>?): Call<DataEntity<List<Blob>>>
+    fun getAccountOwnedBlobs(@Path("account") accountId: String,
+                             @QueryMap query: Map<String, Any>): Call<DataEntity<List<Blob>>>
+
+    @POST("accounts/{account}/blobs")
+    fun creteAccountOwnedBlob(@Path("account") accountId: String,
+                              @Body blob: DataEntity<Blob>): Call<DataEntity<Blob>>
+
+    @GET("blobs/{blob}")
+    fun get(@Path("blobId") blobId: String): Call<DataEntity<Blob>>
+
+    @GET("accounts/{account}/blobs/{blob}")
+    fun getAccountOwnedBlob(@Path("account") accountId: String,
+                            @Path("blob") blobId: String): Call<DataEntity<Blob>>
+
+    @DELETE("blobs/{blob}")
+    fun delete(@Path("blob") blobId: String): Call<Void>
+
+    @POST("blobs")
+    fun create(@Body blob: DataEntity<Blob>): Call<DataEntity<Blob>>
 }
