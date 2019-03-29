57431263e533e2e631bb803ec8f3adbcc90ae143
Oleg Koretsky <test.radiokot@gmail.com>
2019-03-29 15:00:18 +0200

    Added quote_asset and quote_amount fields to MatchedOrder model

--- src/main/java/org/tokend/sdk/api/trades/model/MatchedOrder.kt
+++ src/main/java/org/tokend/sdk/api/trades/model/MatchedOrder.kt
@@ -8,6 +8,8 @@ open class MatchedOrder(
 		@field:SerializedName("id") val id: Int,
 		@field:SerializedName("base_asset") val baseAsset: String,
 		@field:SerializedName("base_amount") val baseAmount: String,
+		@field:SerializedName("quote_asset") val quoteAsset: String,
+		@field:SerializedName("quote_amount") val quoteAmount: String,
 		@field:SerializedName("price") val price: String,
 		@field:SerializedName("created_at") val createdAt: Date
 )
