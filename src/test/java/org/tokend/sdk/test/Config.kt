841fcb5fc6fbcf10e060adaa16a77a1627021f1b
Oleg Koretsky <test.radiokot@gmail.com>
2019-04-04 16:03:58 +0300

    Update test env

--- src/test/java/org/tokend/sdk/test/Config.kt
+++ src/test/java/org/tokend/sdk/test/Config.kt
@@ -3,7 +3,7 @@ package org.tokend.sdk.test
 import org.tokend.wallet.Account
 
 object Config {
-    const val API_URL = "https://api.angus.tokend.io/"
+    const val API_URL = "http://localhost:8000/_/api/"
     const val ADMIN_SEED = "SAMJKTZVW5UOHCDK5INYJNORF2HRKYI72M5XSZCBYAHQHR34FFR4Z6G4"
     val ADMIN_ACCOUNT = Account.fromSecretSeed(ADMIN_SEED.toCharArray())
 }
