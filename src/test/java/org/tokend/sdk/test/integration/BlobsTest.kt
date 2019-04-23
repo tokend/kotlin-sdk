d6b89c91025adabe4e5888acfe80cce206f62c74
Oleg Koretsky <test.radiokot@gmail.com>
2019-04-23 16:23:26 +0300

    Added integration test for blob creation

---
+++ src/test/java/org/tokend/sdk/test/integration/BlobsTest.kt
@@ -0,0 +1,55 @@
+package org.tokend.sdk.test.integration
+
+import org.junit.Assert
+import org.junit.Test
+import org.tokend.sdk.api.blobs.model.Blob
+import org.tokend.sdk.api.blobs.model.BlobType
+import org.tokend.sdk.keyserver.KeyServer
+import org.tokend.sdk.test.Util
+import kotlin.math.absoluteValue
+import kotlin.random.Random
+
+class BlobsTest {
+    @Test
+    fun aCreateBlob() {
+        var email = "blobCreationTest" + Random.nextInt().absoluteValue + "@mail.com"
+        val password = "qwe123".toCharArray()
+
+        val api = Util.getApi()
+
+        val keyServer = KeyServer(api.wallets)
+
+        System.out.println("Attempt to sign up $email ${password.joinToString("")}")
+
+        val (wallet, rootAccount, _)
+                = keyServer.createAndSaveWallet(email, password).execute().get()
+        email = wallet.attributes!!.email
+
+        val currentWalletInfo = keyServer.getWalletInfo(email, password).execute().get()
+
+        val signedApi = Util.getSignedApi(rootAccount, api.rootUrl)
+
+        val content = "Hello World"
+
+        val blob = signedApi
+                .blobs
+                .createAccountOwnedBlob(
+                        accountId = currentWalletInfo.accountId,
+                        blob = Blob(BlobType.ALPHA, content)
+                )
+                .execute()
+                .get()
+
+        val downloadedBlob = signedApi
+                .blobs
+                .getAccountOwnedBlob(
+                        accountId = currentWalletInfo.accountId,
+                        blobId = blob.id
+                )
+                .execute()
+                .get()
+
+        Assert.assertEquals("Downloaded blob content must be the same as the uploaded one",
+                content, downloadedBlob.valueString)
+    }
+}
