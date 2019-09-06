011b8f7261014817032a27dfa02b3f739e8f2037
Oleg Koretsky <test.radiokot@gmail.com>
2019-09-06 14:10:16 +0300

    Added ApiRequest::executeAsync methods accepting lambdas

--- src/main/java/org/tokend/sdk/api/base/ApiRequest.kt
+++ src/main/java/org/tokend/sdk/api/base/ApiRequest.kt
@@ -17,6 +17,21 @@ interface ApiRequest<T> {
     fun executeAsync(callback: ApiCallback<T>)
 
     /**
+     * Executes request asynchronously in the separate thread.
+     */
+    fun executeAsync(onSuccess: (ApiResponse<T>) -> Unit,
+                     onError: ((Throwable) -> Unit)?) = executeAsync(object : ApiCallback<T> {
+        override fun onSuccess(response: ApiResponse<T>) { onSuccess(response) }
+
+        override fun onError(error: Throwable) { onError?.invoke(error) }
+    })
+
+    /**
+     * Executes request asynchronously in the separate thread.
+     */
+    fun executeAsync(onSuccess: (ApiResponse<T>) -> Unit) = executeAsync(onSuccess, null)
+
+    /**
      * Cancels async request execution.
      */
     fun cancel()
