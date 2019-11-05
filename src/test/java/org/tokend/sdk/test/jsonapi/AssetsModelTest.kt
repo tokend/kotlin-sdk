0801b6988e0dc9118ef7bf6a6786f7e389e0757e
Oleg Koretsky <test.radiokot@gmail.com>
2019-11-05 18:52:45 +0200

    Actualized asset model test

--- src/test/java/org/tokend/sdk/test/jsonapi/AssetsModelTest.kt
+++ src/test/java/org/tokend/sdk/test/jsonapi/AssetsModelTest.kt
@@ -21,73 +21,52 @@ class AssetsModelTest {
 
         Assert.assertTrue(asset.isFilled())
         Assert.assertNotNull(asset.details.get("name").asText())
-        Assert.assertTrue(asset.policies.has(AssetPolicy.STATS_QUOTE_ASSET.value))
+        Assert.assertTrue(asset.policies.has(AssetPolicy.BASE_ASSET.value))
         Assert.assertNotNull(asset.owner)
     }
 
-    private val assetResponse = "{  \n" +
-            "   \"data\":{  \n" +
-            "      \"id\":\"USD\",\n" +
-            "      \"type\":\"assets\",\n" +
-            "      \"relationships\":{  \n" +
-            "         \"owner\":{  \n" +
-            "            \"data\":{  \n" +
-            "               \"id\":\"GBSR6JG5AYSAW7HK6EGJFYVIVN54LVGSY3ZLJ6X3IBQZ766EJABCZQTH\",\n" +
-            "               \"type\":\"accounts\"\n" +
-            "            }\n" +
-            "         }\n" +
-            "      },\n" +
-            "      \"attributes\":{  \n" +
-            "         \"pre_issuance_asset_signer\":\"GBSR6JG5AYSAW7HK6EGJFYVIVN54LVGSY3ZLJ6X3IBQZ766EJABCZQTH\",\n" +
-            "         \"max_issuance_amount\":\"1000.000001\",\n" +
-            "         \"available_for_issuance\":\"20.000000\",\n" +
-            "         \"issued\":\"10.000000\",\n" +
-            "         \"type\":\"1\",\n" +
-            "         \"pending_issuance\":\"123.000123\",\n" +
-            "         \"policies\":{  \n" +
-            "            \"value\":4,\n" +
-            "            \"flags\":[  \n" +
-            "               {  \n" +
-            "                  \"name\":\"transfarable\",\n" +
-            "                  \"value\":2\n" +
-            "               }\n" +
-            "            ]\n" +
-            "         },\n" +
-            "         \"trailing_digits\":2,\n" +
-            "           \"details\":{  \n" +
-            "            \"logo\":{  \n" +
-            "               \"key\":\"dpurgh4infnubjhcost7fvjkdwnvkcedflpqdxlxsc5nlsib4diraweq\",\n" +
-            "               \"name\":\"bitcoin_logo.png\",\n" +
-            "               \"type\":\"image/png\"\n" +
-            "            },\n" +
-            "            \"name\":\"Bitcoin\",\n" +
-            "            \"terms\":{  \n" +
-            "               \"key\":\"\",\n" +
-            "               \"name\":\"\",\n" +
-            "               \"type\":\"\"\n" +
-            "            }}}\n" +
-            "   },\n" +
-            "   \"included\":[  \n" +
-            "      {  \n" +
-            "         \"id\":\"GBSR6JG5AYSAW7HK6EGJFYVIVN54LVGSY3ZLJ6X3IBQZ766EJABCZQTH\",\n" +
-            "         \"type\":\"accounts\",\n" +
-            "         \"relationships\":{  \n" +
-            "            \"referrer\":{  \n" +
-            "               \"data\":{  \n" +
-            "                  \"id\":\"GBSR6JG5AYSAW7HK6EGJFYVIVN54LVGSY3ZLJ6X3IBQZ766EJABCZQTH\",\n" +
-            "                  \"type\":\"accounts\"\n" +
-            "               }\n" +
-            "            },\n" +
-            "            \"balances\":{  \n" +
-            "               \"data\":[  \n" +
-            "                  {  \n" +
-            "                     \"id\":\"BDLXKLLVS6UIQEDYCZKIC5222GGQLCOTHM4BWXVCBDQMJTUWUFUATV2Q\",\n" +
-            "                     \"type\":\"balances\"\n" +
-            "                  }\n" +
-            "               ]\n" +
-            "            }\n" +
-            "         }\n" +
-            "      }\n" +
-            "   ]\n" +
+    private val assetResponse = "{\n" +
+            "\"data\": {\n" +
+            "\"id\": \"BTC\",\n" +
+            "\"type\": \"assets\",\n" +
+            "\"attributes\": {\n" +
+            "\"available_for_issuance\": \"21000000.000000\",\n" +
+            "\"details\": {\n" +
+            "\"name\": \"Bitcoin\"\n" +
+            "},\n" +
+            "\"issued\": \"0.000000\",\n" +
+            "\"max_issuance_amount\": \"21000000.000000\",\n" +
+            "\"pending_issuance\": \"0.000000\",\n" +
+            "\"policies\": {\n" +
+            "\"value\": 3,\n" +
+            "\"flags\": [\n" +
+            "{\n" +
+            "\"name\": \"transferable\",\n" +
+            "\"value\": 1\n" +
+            "},\n" +
+            "{\n" +
+            "\"name\": \"base_asset\",\n" +
+            "\"value\": 2\n" +
+            "}\n" +
+            "]\n" +
+            "},\n" +
+            "\"pre_issuance_asset_signer\": \"GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB\",\n" +
+            "\"state\": {\n" +
+            "\"name\": \"active\",\n" +
+            "\"value\": 0\n" +
+            "},\n" +
+            "\"trailing_digits\": 6,\n" +
+            "\"type\": 0\n" +
+            "},\n" +
+            "\"relationships\": {\n" +
+            "\"owner\": {\n" +
+            "\"data\": {\n" +
+            "\"id\": \"GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB\",\n" +
+            "\"type\": \"accounts\"\n" +
+            "}\n" +
+            "}\n" +
+            "}\n" +
+            "},\n" +
+            "\"included\": []\n" +
             "}"
 }
