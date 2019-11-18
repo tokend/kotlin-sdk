11823c88b43dcfce012f970f28d02e2f42c7f9f2
Oleg Koretsky <test.radiokot@gmail.com>
2019-11-18 13:32:21 +0200

    Made base64 string extensions tolerant to missing paddings

--- src/main/java/org/tokend/sdk/utils/extentions/Encoding.kt
+++ src/main/java/org/tokend/sdk/utils/extentions/Encoding.kt
@@ -1,12 +1,13 @@
 package org.tokend.sdk.utils.extentions
 
-import org.spongycastle.util.encoders.Base64
 import org.spongycastle.util.encoders.Hex
+import org.tokend.wallet.utils.Base64
+import java.nio.charset.Charset
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
-fun String.decodeBase64(): ByteArray {
-    return Base64.decode(this)
+fun String.decodeBase64(charset: Charset = Charsets.UTF_8): ByteArray {
+    return Base64.decode(this.toByteArray(charset))
 }
 
 fun String.decodeHex(): ByteArray {
@@ -17,8 +18,8 @@ fun ByteArray.encodeBase64(): ByteArray {
     return Base64.encode(this)
 }
 
-fun ByteArray.encodeBase64String(): String {
-    return String(Base64.encode(this))
+fun ByteArray.encodeBase64String(charset: Charset = Charsets.UTF_8): String {
+    return String(Base64.encode(this), charset)
 }
 
 fun ByteArray.decodeBase64(): ByteArray {
