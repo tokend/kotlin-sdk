eaede9adce06ea8f17b7e603e131c64ed4399e3a
Oleg Koretsky <test.radiokot@gmail.com>
2020-08-17 11:44:29 +0300

    Actualize invoices integration API

--- src/main/java/org/tokend/sdk/api/integrations/invoices/model/generated/resources/AllResources.java
+++ src/main/java/org/tokend/sdk/api/integrations/invoices/model/generated/resources/AllResources.java
@@ -5,6 +5,7 @@ package org.tokend.sdk.api.integrations.invoices.model.generated.resources;
 
 public class AllResources {
     public static final Class<?>[] ARRAY = {
+        EventResource.class,
         DataResource.class,
         InvoiceResource.class
     };
