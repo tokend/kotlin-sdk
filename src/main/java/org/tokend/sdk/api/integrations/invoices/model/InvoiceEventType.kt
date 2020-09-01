bec789ffdf7cefea351553d131e5fd544a26cd36
Oleg Koretsky <test.radiokot@gmail.com>
2020-09-01 12:49:13 +0300

    Fix invoice event type enum values shift

--- src/main/java/org/tokend/sdk/api/integrations/invoices/model/InvoiceEventType.kt
+++ src/main/java/org/tokend/sdk/api/integrations/invoices/model/InvoiceEventType.kt
@@ -1,18 +1,19 @@
 package org.tokend.sdk.api.integrations.invoices.model
 
-enum class InvoiceEventType(val value: Int) {
-    CREATED(1),
-    UPDATED(2),
-    REJECTED(3),
-    CANCELLED(4),
-    COMPLETED(5);
+enum class InvoiceEventType {
+    CREATED,
+    UPDATED,
+    REJECTED,
+    CANCELLED,
+    COMPLETED;
+
+    val value: Int
+        get() = ordinal
 
     companion object {
-        private val valuesMap = values().associateBy(InvoiceEventType::value)
-
         @JvmStatic
         fun fromValue(value: Int): InvoiceEventType {
-            return valuesMap[value]
+            return values().getOrNull(value)
                     ?: throw IllegalArgumentException("There is no invoice event type with value '$value'")
         }
 
