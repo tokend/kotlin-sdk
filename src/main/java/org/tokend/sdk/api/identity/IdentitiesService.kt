bf3bf8ba613aca58d54270d932a2fa95a2d86e37
KostyaNechvolod <43446812+KostyaNechvolod@users.noreply.github.com>
2020-10-19 12:19:13 +0300

    Implement passportId in identities API (#15)
    Co-authored-by: Kostiantyn Nechvolod <kostiantyn.nechvolod@gmail.com>

--- src/main/java/org/tokend/sdk/api/identity/IdentitiesService.kt
+++ src/main/java/org/tokend/sdk/api/identity/IdentitiesService.kt
@@ -3,11 +3,7 @@ package org.tokend.sdk.api.identity
 import com.github.jasminb.jsonapi.JSONAPIDocument
 import org.tokend.sdk.api.base.model.AttributesEntity
 import org.tokend.sdk.api.base.model.DataEntity
-import org.tokend.sdk.api.identity.model.IdentityResource
-import org.tokend.sdk.api.identity.model.MassEmailAccountKey
-import org.tokend.sdk.api.identity.model.SetPhoneRequestAttributes
-import org.tokend.sdk.api.identity.model.SetTelegramRequestAttributes
-import org.tokend.sdk.api.identity.model.IdentitySettingsResource
+import org.tokend.sdk.api.identity.model.*
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -29,6 +25,11 @@ interface IdentitiesService {
     @JvmSuppressWildcards
     fun setPhone(@Path("accountId") accountId: String,
                  @Body body: DataEntity<AttributesEntity<SetPhoneRequestAttributes>>): Call<Void>
+
+    @PUT("identities/{accountId}/settings/passport")
+    @JvmSuppressWildcards
+    fun setPassportId(@Path("accountId") accountId: String,
+                 @Body body: DataEntity<AttributesEntity<SetPassportIdRequestAttributes>>): Call<Void>
 
     @PUT("identities/{accountId}/settings/telegram")
     @JvmSuppressWildcards
