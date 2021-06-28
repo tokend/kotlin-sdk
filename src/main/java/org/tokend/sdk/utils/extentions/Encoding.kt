97549be246d99fdd8063e6293631ac79b5990b7e
Oleg Koretsky <test.radiokot@gmail.com>
2021-06-28 17:22:46 +0300

    Remove useless encoding params and hash() method from Encoding util

--- src/main/java/org/tokend/sdk/utils/extentions/Encoding.kt
+++ src/main/java/org/tokend/sdk/utils/extentions/Encoding.kt
@@ -2,15 +2,20 @@ package org.tokend.sdk.utils.extentions
 
 import org.spongycastle.util.encoders.Hex
 import org.tokend.wallet.utils.Base64
-import java.nio.charset.Charset
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
 
-fun String.decodeBase64(charset: Charset = Charsets.UTF_8): ByteArray {
-    return Base64.decode(this.toByteArray(charset))
+fun String.decodeBase64(): ByteArray {
+    return this.toByteArray(Charsets.UTF_8).decodeBase64()
+}
+
+fun ByteArray.decodeBase64(): ByteArray {
+    return Base64.decode(this)
 }
 
 fun String.decodeHex(): ByteArray {
+    return Hex.decode(this)
+}
+
+fun ByteArray.decodeHex(): ByteArray {
     return Hex.decode(this)
 }
 
@@ -18,26 +23,8 @@ fun ByteArray.encodeBase64(): ByteArray {
     return Base64.encode(this)
 }
 
-fun ByteArray.encodeBase64String(charset: Charset = Charsets.UTF_8): String {
-    return String(Base64.encode(this), charset)
-}
-
-fun ByteArray.decodeBase64(): ByteArray {
-    return Base64.decode(this)
-}
-
-/**
- * Returns SHA-256 hash of `data`.
- * @param data
- */
-fun ByteArray.hash(): ByteArray {
-    return try {
-        val md = MessageDigest.getInstance("SHA-256")
-        md.update(this)
-        md.digest()
-    } catch (e: NoSuchAlgorithmException) {
-        throw RuntimeException("SHA-256 not implemented")
-    }
+fun ByteArray.encodeBase64String(): String {
+    return String(Base64.encode(this), Charsets.UTF_8)
 }
 
 fun ByteArray.encodeHex(): ByteArray {
@@ -45,9 +32,5 @@ fun ByteArray.encodeHex(): ByteArray {
 }
 
 fun ByteArray.encodeHexString(): String {
-    return String(Hex.encode(this))
-}
-
-fun ByteArray.decodeHex(): ByteArray {
-    return Hex.decode(this)
+    return String(Hex.encode(this), Charsets.UTF_8)
 }
